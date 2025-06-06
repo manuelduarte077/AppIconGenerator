e97f843c85f2dd8bd360af077961c0598a136365
manuelduarte077 <carlosd99m@gmail.com>
2025-06-06 04:16:25 -0600

    feat: implement app icon generator with image picker

--- DevIconGenerator/DevIconGeneratorApp.swift
+++ DevIconGenerator/DevIconGeneratorApp.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 @main
-struct DevIconGeneratorApp: App {
+struct XCAIconGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
+            #if targetEnvironment(macCatalyst)
             ContentView()
+                .onAppear {
+                    guard let scenes = UIApplication.shared.connectedScenes as? Set<UIWindowScene> else { return }
+                    for window in scenes {
+                        window.title = "DEV Icon Generator"
+                        guard let sizeRestrictions = window.sizeRestrictions else { continue }
+                        sizeRestrictions.minimumSize = CGSize(width: 360, height: 600)
+                        sizeRestrictions.maximumSize = sizeRestrictions.minimumSize
+                    }
+                }
+            #else
+            NavigationView {
+                ContentView()
+                    .navigationTitle("DEV Icon Gen")
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            #endif
         }
     }
 }
