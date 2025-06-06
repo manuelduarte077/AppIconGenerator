0439706addb563cf17a88dc413e2ef50546777c3
manuelduarte077 <carlosd99m@gmail.com>
2025-06-06 03:24:36 -0600

    Initial Commit

---
+++ DevIconGenerator/DevIconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevIconGeneratorApp.swift
+//  DevIconGenerator
+//
+//  Created by Manuel Duarte on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DevIconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
